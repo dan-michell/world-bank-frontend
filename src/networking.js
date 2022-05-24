a2247146f6a7ad94fea02ec47ddf67159c954b70
Dan Michell <dan.michell1000@gmail.com>
2022-05-24 12:21:35 +0100

    Added registration, login and logout methods to networking class

--- src/networking.js
+++ src/networking.js
@@ -1,1 +1,60 @@
-export default class Networking {}
+export default class Networking {
+  async registerUser(email, username, password, passwordConformation) {
+    try {
+      const response = await fetch("http://localhost:8080/users", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, username, password, passwordConformation }),
+      });
+      const json = await response.json();
+      return json.response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async loginUser(username, password) {
+    try {
+      const response = await fetch("http://localhost:8080/sessions", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const json = await response.json();
+      return json;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async logoutUser() {
+    try {
+      const response = await fetch("http://localhost:8080/sessions", {
+        method: "DELETE",
+        credentials: "include",
+      });
+      const json = await response.json();
+      return json.response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async getUser() {
+    try {
+      const response = await fetch("http://localhost:8080/sessions", {
+        credentials: "include",
+      });
+      const user = await response.json();
+      return user;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
