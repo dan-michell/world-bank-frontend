1d7b3d93aa39cdb24d058e0ad49b68cce9a8cb2d
Dan Michell <dan.michell1000@gmail.com>
2022-05-26 16:38:40 +0100

    Single country data graph working

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,11 +1,54 @@
-import { useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
 function Results(props) {
-  useEffect(() => {
-    console.log(props.countryData);
-  }, [props.countryData]);
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-  return <div></div>;
+  const labels = props.countryData.map((element) => {
+    return element.year;
+  });
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: props.countryData[0].countryname,
+        data: props.countryData.map((element) => {
+          return element.value;
+        }),
+        backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+        borderColor: ["rgb(255, 99, 132)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: props.countryData[0].indicatorname,
+      },
+    },
+  };
+
+  return (
+    <div className="flex flex-col m-28 w-[80%] h-[80%] items-center justify-center">
+      <Line data={data} options={options} />
+    </div>
+  );
 }
 
 export default Results;
